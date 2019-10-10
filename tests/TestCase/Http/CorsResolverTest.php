816fb69f99c3b2e973cd40923d75ed6387aec432
Jason Horvath <jhorvath@medforce.net>
2019-10-10 09:54:43 -0400

    Functional tests added

--- tests/TestCase/Http/CorsResolverTest.php
+++ tests/TestCase/Http/CorsResolverTest.php
@@ -1,12 +1,19 @@
 <?php
-
+/**
+ * Cors Control: A CakePHP plugin for adding CORS headers
+ * 
+ * @author      Jason Horvah <jason.horvath@greaterdevelopment.com>
+ * @link        https://greaterdevelopment.com
+ * @since       1.0.0
+ * @license     https://opensource.org/licenses/mit-license.php MIT License
+ */
 namespace CorsControl\Test\TestCase;
 
-use Cake\Http\Client\Response;
+use Cake\Http\Response;
 use Cake\Http\ServerRequest;
 use Cake\TestSuite\TestCase;
 use CorsControl\Http\CorsResolver;
-use ReflectionClass;
+use VisibleUnit\Reflections\BaseReflection;
 
 class CorsResolverTest extends TestCase
 {
@@ -27,7 +34,7 @@ class CorsResolverTest extends TestCase
     protected $corsResolver;
 
     /**
-     * @var ReflectionClass
+     * @var \VisibleUnit\Reflections\BaseReflection
      */
     protected $reflection;
 
@@ -42,7 +49,7 @@ class CorsResolverTest extends TestCase
         $this->request = new ServerRequest();
         $this->response = new Response();
         $this->corsResolver = new CorsResolver($this->request, $this->response);
-        $this->reflection = new ReflectionClass($this->corsResolver);
+        $this->reflection = new BaseReflection($this->corsResolver);
     }
 
     /**
@@ -52,12 +59,7 @@ class CorsResolverTest extends TestCase
      */
     public function testSetConfig()
     {
-        $property = $this->reflection->getProperty('config');
-        $property->setAccessible(true);
-        $method = $this->reflection->getMethod('setConfig');
-        $method->setAccessible(true);
-
-        $value = $property->getValue($this->corsResolver);
+        $value = $this->reflection->getPropValue('config');
         $this->assertSame('*', $value['allowOrigin'][0]);
 
         $testConfig = [
@@ -65,8 +67,8 @@ class CorsResolverTest extends TestCase
             'allowMethods' => ['TEST', 'TESTAGAIN'],
             'invalidPropShouldBeNull' => 'This should not be here after invoke'
         ];
-        $method->invoke($this->corsResolver, $testConfig);
-        $value = $property->getValue($this->corsResolver);
+        $this->reflection->invokeMethod('setConfig', $testConfig);
+        $value = $this->reflection->getPropValue('config');
         $this->assertSame('invoked', $value['allowOrigin'][0]);
         $this->assertCount(2, $value['allowMethods']);
         $this->assertSame('TESTAGAIN', $value['allowMethods'][1]);
@@ -80,19 +82,110 @@ class CorsResolverTest extends TestCase
      */
     public function testSetConfigValue()
     {
-        $property = $this->reflection->getProperty('config');
-        $property->setAccessible(true);
-        $method = $this->reflection->getMethod('setConfigValue');
-        $method->setAccessible(true);
-
-
-        $config = $property->getValue($this->corsResolver);
-        $method->invoke($this->corsResolver, 'allowOrigin', ['test', 'testmore']);
-        $method->invoke($this->corsResolver, 'allowCredentials', false);
-        $config = $property->getValue($this->corsResolver);
+        $config = $this->reflection->getPropValue('config');
+        $this->reflection->invokeMethod('setConfigValue', 'allowOrigin', ['test', 'testmore']);
+        $this->reflection->invokeMethod('setConfigValue', 'allowCredentials', false);
+        $config = $this->reflection->getPropValue('config');
         $this->assertSame('testmore', $config['allowOrigin'][1]);
         $this->assertFalse($config['allowCredentials']);
         $test = '';
     }
 
+    /**
+     * Test Get Builder Response
+     * 
+     * @return void
+     */
+    public function testGetBuilderResponse()
+    {
+        $response = $this->reflection->invokeMethod('getBuilderResponse');
+        $this->assertInstanceOf('Cake\Http\Response', $response);
+    }
+    
+    /**
+     * Test Get Builder
+     * 
+     * @return void
+     */
+    public function testGetBuilder()
+    {
+        $builder = $this->reflection->invokeMethod('getBuilder');
+        $this->assertInstanceOf('Cake\Http\CorsBuilder', $builder);
+    }
+
+    /**
+     * Test With Builder Methods
+     * 
+     * @return void
+     */
+    public function testWithBuilderMethods()
+    {
+        // with default headers
+        $builderReflection = $this->getNewTestBuilderReflection();
+        $corsConfig = $this->reflection->getPropValue('config');
+        $builderHeaders = $builderReflection->getPropValue('_headers');
+        $this->corsToHeaderAssertions($corsConfig, $builderHeaders);
+
+        // with altered headers
+        $corsConfigPassHeaders = [
+            'allowOrigin' => ['example.com'],
+            'allowMethods' => [   
+                'OPTIONS',
+                'PATCH',
+            ],
+            'allowHeaders' => ['TEST-HEADER', 'TEST-HEADER2'],
+            'exposeHeaders' => ['CHANGED'],
+            'allowCredentials' => false,
+            'maxAge' => 500
+        ];
+
+        $this->reflection->invokeMethod('setConfig', $corsConfigPassHeaders);
+        $builderReflection = $this->getNewTestBuilderReflection();
+        $corsConfig = $this->reflection->getPropValue('config');
+        $builderHeaders = $builderReflection->getPropValue('_headers');
+        $this->corsToHeaderAssertions($corsConfig, $builderHeaders);
+    }
+
+    /**
+     * Get Test Builder
+     * 
+     * @return void
+     */
+    protected function getTestBuilder()
+    {
+        $builder = $this->reflection->invokeMethod('getBuilder');
+        return $this->reflection->invokeMethod('withBuilderMethods', $builder);
+    }
+
+    /**
+     * Get new Test Builder Reflection
+     * 
+     * @return void
+     */
+    protected function getNewTestBuilderReflection()
+    {
+        $testBuilder = $this->getTestBuilder();
+        return new BaseReflection($testBuilder);
+    }
+    /**
+     * Cors To Header Assertions
+     * 
+     * @param array $corsConfig
+     * @param array $builderHeaders
+     * @return void
+     */
+    protected function corsToHeaderAssertions(array $corsConfig, array $builderHeaders)
+    {
+        $this->assertSame(implode(', ', $corsConfig['allowMethods']), $builderHeaders['Access-Control-Allow-Methods']);
+        $this->assertSame(implode(', ', $corsConfig['allowHeaders']), $builderHeaders['Access-Control-Allow-Headers']);
+        $this->assertSame(implode(', ', $corsConfig['exposeHeaders']), $builderHeaders['Access-Control-Expose-Headers']);
+        $this->assertSame($corsConfig['maxAge'], $builderHeaders['Access-Control-Max-Age']);
+
+        // 'Access-Control-Allow-Credentials' header should not be present when config 'allowCredentials' set to false
+        if ($corsConfig['allowCredentials']) {
+            $this->assertSame('true', $builderHeaders['Access-Control-Allow-Credentials']);
+        } else {
+            $this->assertFalse(isset($builderHeaders['Access-Control-Allow-Credentials']));
+        }
+    }
 }
